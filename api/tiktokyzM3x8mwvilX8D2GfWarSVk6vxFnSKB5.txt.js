7d709fc0e99a9343e37fe29ded53881c703185b2
Hind Aaibid <aaibidhindo@gmail.com>
2026-03-19 15:57:20 +0100

    Fix TikTok verification static file

--- api/tiktokyzM3x8mwvilX8D2GfWarSVk6vxFnSKB5.txt.js
+++
@@ -1,6 +0,0 @@
-export default function handler(req, res) {
-  res.setHeader("Content-Type", "text/plain");
-  res.status(200).send(
-    "tiktok-developers-site-verification=yzM3x8mwvilX8D2GfWarSVk6vxFnSKB5"
-  );
-}
